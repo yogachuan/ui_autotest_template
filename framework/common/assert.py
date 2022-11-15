97c7958529473222b3c74685a847024e86c83f3d
yogachuan <1010474808@qq.com>
2022-11-15 11:47:04 +0800

    修改了测试报告保存路径及readme

--- framework/common/assert.py
+++
@@ -1,8 +0,0 @@
-from selenium.webdriver.common.by import By
-from framework.common.driver import Browser
-from framework.common.basepage import BasePage
-
-
-class MyAssert(BasePage):
-    def __init__(self, dr):
-        super().__init__(dr)
